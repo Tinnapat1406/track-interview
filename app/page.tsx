f400899bcd1a265c0097366c1a68890293a2669b
Tinnapat1406 <tinnapatz2548@gmail.com>
2026-08-19 15:47:16 +0700

    edit a little bit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <h1 className="max-x-md text-3xl font-semibold leading-20">
+        Interview Track{""}
+        </h1>
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-left justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h3 className="text-3xl font-semibold text-left leading-10 tracking-tight text-black dark:text-zinc-50">
+          Candidate Page
+          </h3>
         <Image
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
@@ -12,16 +18,18 @@ export default function Home() {
           height={20}
           priority
         />
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
+            candidate{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
               page.tsx
             </code>{" "}
             file.
+          
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Candidate{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -38,6 +46,7 @@ export default function Home() {
             center.
           </p>
         </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
