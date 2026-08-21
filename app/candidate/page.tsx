56a3a6c946db076a85dbe9cc30bc470549424570
Tinnapat1406 <tinnapatz2548@gmail.com>
2026-08-21 10:24:10 +0700

    fix someting

---
+++ app/candidate/page.tsx
@@ -0,0 +1,10 @@
+export default function candidate() {
+    return(
+        <div>
+            <h1 className="text-3xl font-semibold text-start leading-10 tracking-tight text-black dark:text-zinc-50">
+                Candidates
+            </h1>
+        </div>
+
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="max-x-md text-3xl font-semibold leading-20">
-        Interview Track{""}
-        </h1>
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-left justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h3 className="text-3xl font-semibold text-left leading-10 tracking-tight text-black dark:text-zinc-50">
-          Candidate Page
-          </h3>
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+      <h1 className="max-w-md text-3xl font-semibold text-center leading-20">
+        Interview Track
+      </h1>
+      <main className="text-left flex flex-1 w-full max-w-3xl flex-col items-start justify-between py-32 px-16 bg-white dark:bg-black">
+       
+      <nav>
+        <Link href="/candidate">candidate</Link>
+      </nav>
+        <div className="flex flex-col items-start gap-6 text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            candidate{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          
+             Go to candidate{" "}
           </h1>
+
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Candidate{" "}
             <a
@@ -74,5 +64,8 @@ export default function Home() {
         </div>
       </main>
     </div>
+    
   );
 }
+
+
