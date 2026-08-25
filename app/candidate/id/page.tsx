3c7ceb3d61f4894685270928c854b4db4b553f6f
Tinnapat1406 <tinnapatz2548@gmail.com>
2026-08-25 12:51:27 +0700

    route to dashboard

--- app/candidate/id/page.tsx
+++ app/candidate/id/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Identity(){
     return(
         <div>
-            <h1>Candidate Identity</h1>
+            <h1 className="text-3xl font-semibold text-center leading-10 tracking-tight text-black dark:text-zinc-50">
+                Candidate Identity
+            </h1>
+
+            <Link href="/dashboard" className="flex items-start mt-4">
+                <p className="font-semibold text-lg text-left leading-10 tracking-tight text-black dark:text-zinc-50">
+                    Back to Dashboard
+                </p>
+            </Link>
         </div>
     )
 }
